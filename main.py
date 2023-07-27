75fea7ab80de68563903525443bc2426d0493e81
Maazin Haque <maahaque@publicisgroupe.net>
2023-07-27 14:53:14 +0400

    Tenth commit

--- main.py
+++ main.py
@@ -12,18 +12,11 @@ st.set_page_config(
     initial_sidebar_state="expanded")
 
 # Add custom CSS to hide the GitHub icon
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+hide_github_icon = """
+
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 
 for k, v in st.session_state.items():
     st.session_state[k] = v
